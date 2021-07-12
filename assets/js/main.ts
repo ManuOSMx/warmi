96c70bbbe711152dd0dbb3bbd9a8d6b297ccb6ae
Manuel Ortiz <manuel4612@hotmail.com>
2021-07-11 19:47:10 -0500

    agrege MLSA

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,3 +1,4 @@
 const edith1 = document.getElementById('editar');
+var userName: string = "Todos los Student Ambassadors";
 
-edith1.innerHTML = "¿Como estan en Warmi?";
+edith1.innerHTML = `¿Como estan en Warmi? Los saluda ${userName}` ;
