e4c37dc661c700649ff06dacd45efb92ce0d3c9f
Manuel Ortiz <manuel4612@hotmail.com>
2021-07-11 19:51:48 -0500

    v2

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,4 +1,4 @@
 const edith1 = document.getElementById('editar');
-var userName: string = "Manu";
+var userName: string = "Hello";
 
 edith1.innerHTML = `¿Como estan en Warmi? Los saluda ${userName}` ;
